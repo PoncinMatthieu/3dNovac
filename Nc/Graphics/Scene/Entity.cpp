0e98165d3afe8d03079963d6b7225ba123b37bed
Poncin Matthieu <poncin.matthieu@gmail.com>
2011-10-20 22:10:30 +0000

    [Portage Windows] Add missing Entity.h/.cpp

---
+++ Nc/Graphics/Scene/Entity.cpp
@@ -0,0 +1,188 @@
+
+/*-----------------------------------------------------------------------------
+
+	3dNovac Graphics
+	Copyright (C) 2010-2011, The 3dNovac Team
+
+    This file is part of 3dNovac.
+
+    3dNovac is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3dNovac is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with 3dNovac.  If not, see <http://www.gnu.org/licenses/>.
+
+    File Created At:        16/10/2011
+    File Author(s):         Poncin Matthieu
+
+-----------------------------------------------------------------------------*/
+
+#include "Entity.h"
+
+using namespace Nc;
+using namespace Nc::Graphic;
+
+Entity::Entity()
+    : NodePolitic(ClassName())
+{
+}
+
+Entity::Entity(const char *className)
+    : NodePolitic(className)
+{
+}
+
+Entity::Entity(const char *className, const TMatrix &m)
+    : NodePolitic(className), Matrix(m)
+{}
+
+Entity::Entity(const char *className, const Box3f &box)
+    : NodePolitic(className), _box(box)
+{}
+
+Entity::Entity(const char *className, const Box3f &box, const TMatrix &m)
+    : NodePolitic(className), _box(box), Matrix(m)
+{}
+
+Entity::Entity(const Entity &n)
+    : NodePolitic(n)
+{
+    Matrix = n.Matrix;
+    _box = n._box;
+    if (NodePolitic::Data != NULL)
+        NodePolitic::Data = n.Data->Clone();
+}
+
+Entity &Entity::operator = (const Entity &n)
+{
+    NodePolitic::operator = (n);
+    Matrix = n.Matrix;
+    _box = n._box;
+    if (NodePolitic::Data != NULL)
+        NodePolitic::Data = n.Data->Clone();
+    return *this;
+}
+
+Entity::~Entity()
+{
+    if (Data != NULL)
+        delete Data;
+}
+
+void    Entity::RenderChilds(SceneGraph *scene)
+{
+    try
+    {
+        if (NodeType::Data != NULL)
+            NodeType::Data->Render(scene);
+        for (ContainerType::iterator it = _childs.begin(); it != _childs.end(); ++it)
+            (*it)->Render(scene);
+    }
+    catch (const std::exception &e)
+    {
+        LOG_ERROR << "Error in the node render pass: " << e.what() << std::endl;
+    }
+}
+
+void    Entity::UpdateChilds(float elapsedTime)
+{
+    try
+    {
+        if (NodeType::Data != NULL)
+            NodeType::Data->Update(elapsedTime);
+        for (ContainerType::iterator it = _childs.begin(); it != _childs.end(); ++it)
+            (*it)->Update(elapsedTime);
+    }
+    catch (const std::exception &e)
+    {
+        LOG_ERROR << "Error in the node update pass: " << e.what() << std::endl;
+    }
+}
+
+void    Entity::Move(ISceneNode *node, int at, ISceneNode *oldParent, int oldAt)
+{
+
+    Entity *oldParentEntity = NULL;
+    if (oldParent != NULL)
+    {
+        oldParentEntity = dynamic_cast<Entity*>(oldParent);
+        if (oldParentEntity == NULL)
+            throw Utils::Exception("Entity::Move", "Can't move the node from an incompatible parent type.");
+    }
+
+    // insert
+    Lock();
+    Entity *entity = dynamic_cast<Entity*>(node);
+    if (entity != NULL)
+    {
+        if (at < 0)
+            AddChild(entity);
+        else
+            InsertChild(entity, at);
+    }
+    else
+    {
+        Unlock();
+        throw Utils::Exception("Entity::Move", "Can't move the node. The types are incompatible.");
+    }
+    Unlock();
+
+    // remove
+    if (oldParentEntity != NULL)
+    {
+        oldParent->Lock();
+        oldParentEntity->RemoveChild((oldAt > at && oldParentEntity == this) ? (oldAt + 1) : oldAt);
+        oldParent->Unlock();
+    }
+}
+
+void    Entity::Render(SceneGraph *scene)
+{
+    if (_enabled)
+    {
+        _mutex.Lock();
+        RenderChilds(scene);
+        _mutex.Unlock();
+    }
+}
+
+void    Entity::Update(float elapsedTime)
+{
+    if (_enabled)
+        UpdateChilds(elapsedTime);
+}
+
+void    Entity::GetReelBox(Box3f &box) const
+{
+    box = _box;
+    box.Transform(Matrix);
+}
+
+void    Entity::HeightScale(float height)
+{
+    Box3f b = _box; // create a temporary box to use the current matrix
+    if (b.Length(2) != 0)
+    {
+        b.Transform(Matrix);
+        // scale the matrix to obtain the good value
+        Matrix.AddScale(height / b.Length(2));
+    }
+}
+
+void    Entity::CenterBase(const Vector3f &centerBase)
+{
+    Box3f b = _box;
+    b.Transform(Matrix);
+
+    Vector3f center = b.Center();
+    center.Data[2] = b.Min(2);
+    Matrix.AddTranslation(centerBase - center);
+}
+
---
+++ Nc/Graphics/Scene/Entity.h
@@ -0,0 +1,106 @@
+
+/*-----------------------------------------------------------------------------
+
+	3dNovac Graphics
+	Copyright (C) 2010-2011, The 3dNovac Team
+
+    This file is part of 3dNovac.
+
+    3dNovac is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3dNovac is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with 3dNovac.  If not, see <http://www.gnu.org/licenses/>.
+
+    File Created At:        16/10/2011
+    File Author(s):         Poncin Matthieu
+
+-----------------------------------------------------------------------------*/
+
+#ifndef NC_GRAPHICS_SCENE_ENTITY_H_
+#define NC_GRAPHICS_SCENE_ENTITY_H_
+
+#include "SceneNode.h"
+
+namespace Nc
+{
+    namespace Graphic
+    {
+        /// Define an entity node
+        /**
+            An entity is define by a Matrix and can contain only one subtree and a list of childs
+        */
+        class LGRAPHICS Entity : public AbstractSceneNode<Graph::ListNodePolitic>
+        {
+            public:
+                NC_UTILS_DEFINE_PARENT_CLASS(ISceneNode);
+                NC_UTILS_DEFINE_VISITABLE(System::Object);
+                NC_UTILS_DEFINE_INVOKABLE(System::Object);
+                typedef AbstractSceneNode<Graph::ListNodePolitic>            NodePolitic;
+
+            public:
+                Entity();
+                Entity(const char *className);
+                Entity(const char *className, const TMatrix &m);
+                Entity(const char *className, const Box3f &box);
+                Entity(const char *className, const Box3f &box, const TMatrix &m);
+                Entity(const Entity &n);
+                Entity &operator = (const Entity &n);
+                virtual ~Entity();
+
+                static const char           *ClassName()                                        {return "Entity";}
+                virtual ISceneNode          *Clone() const                                      {return new Entity(*this);}
+
+                /** Render the node */
+                virtual void                Render(SceneGraph *scene);
+                /** Update the node */
+                virtual void                Update(float elapsedTime);
+
+                /** \return the number of subtree witch can contain a least one subtree */
+                virtual unsigned int        SubTreeCount() const                                {return ((Data == NULL) ? 0 : 1);}
+                /** \return the subtree */
+                virtual const ISceneNode    *SubTree(unsigned int) const                        {return Data;}
+
+                /**
+                    Insert the given \p node into the current node at the given position \p at and remode it from the given \p oldParent at the given \p oldAt.
+                    Throw an exception if the type of the given \p node is imcompatible with the current node. (Entity and Subtree are incompatible)
+                */
+                virtual void                Move(ISceneNode *node, int at, ISceneNode *oldParent, int oldAt);
+
+                /** Render the childs and the subtrees */
+                void                        RenderChilds(SceneGraph *scene);
+                /** Update the childs and the subtrees */
+                void                        UpdateChilds(float elapsedTime);
+
+                /** Set the box */
+                inline void         SetBox(const Box3f &box)            {_box = box;}
+                /** \return the box (the box is not modified by the matrix, so if you want the reel box call the method GetReelBox) */
+                inline const Box3f  &GetBox() const                     {return _box;}
+                /** \param box filled by the box transformed by the model matrix */
+                void                GetReelBox(Box3f &box) const;
+
+                // operations sur la matrice
+                /** Scale the object with the given height */
+                void                HeightScale(float height); // /!\ metre a jour la bounding box avant
+                /** Translate the model Matrix to the given vector using the xy center of the box */
+                void                CenterBase(const Vector3f &centerBase);
+
+            public:
+                TMatrix                     Matrix;                 ///< the matrix of the entity
+
+            protected:
+                Box3f                       _box;                   ///< the box of the entity
+        };
+    }
+}
+
+
+#endif
+
